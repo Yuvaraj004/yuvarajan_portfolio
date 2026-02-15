ce3a31d4f695dc594bdf443f7ff3f8f94f44b3ac
Yuvaraj004 <yuviyuvaraj0427@gmail.com>
2026-02-15 18:05:01 +0530

    Update basePath for new repo yuvarajan_portfolio

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/Portfolio_yuvarajan' : '',
+  basePath: isProd ? '/yuvarajan_portfolio' : '',
   images: {
     unoptimized: true,
   },
